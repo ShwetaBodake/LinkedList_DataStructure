48d4cd0221a93098105f668051d08ab5f4821302
ShwetaBodake <shwetabodake8108@gmail.com>
2023-01-07 22:58:20 +0530

    Added Elements in 56-30-70 order

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace LinkedList
+{
+public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList List = new LinkedList();
+            List.Add(56);
+            List.Add(30);
+            List.Add(70);
+            List.toprint();
+        }
+    }
+}
