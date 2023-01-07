17f9097521b25069c0449001b6483e188dcbf2e8
ShwetaBodake <shwetabodake8108@gmail.com>
2023-01-07 23:01:21 +0530

    Program of linked list

---
+++ LinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace LinkedList
+{
+	public class LinkedList
+	{
+		public Node head;
+		public void Add(int data)
+		{
+			Node node = new Node(data);
+			if(this.head==null)
+			{
+				this.head = node;
+			}
+			else
+			{
+				Node temp = head;
+				while(temp.next!=null)
+				{
+					temp = temp.next;
+				}
+				temp.next = node;
+			}
+			Console.WriteLine("{0} inserted into linked list", node.data);
+		}
+
+
+		public void toprint()
+		{
+			Node temp = this.head;
+			if(temp==null)
+			{
+				Console.WriteLine("Linked List is empty");
+				return;
+			}
+			while(temp!=null)
+			{
+				Console.Write(temp.data + " ");
+				temp = temp.next;
+			}
+		}
+	}
+}
+
