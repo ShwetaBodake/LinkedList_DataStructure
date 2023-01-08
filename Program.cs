b7992b4f624641930bf1204976c96129740aef0f
ShwetaBodake <shwetabodake8108@gmail.com>
2023-01-08 12:10:34 +0530

    Elements added linked list

--- Program.cs
+++ Program.cs
@@ -6,9 +6,15 @@ public class Program
         public static void Main(string[] args)
         {
             LinkedList List = new LinkedList();
-            List.Add(56);
-            List.Add(30);
+
+
             List.Add(70);
+            List.Add(30);
+            List.Add(56);
+
+            //List.Add(56);
+            //List.Add(30);
+            //List.Add(70);
             List.toprint();
         }
     }
